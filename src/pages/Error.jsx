3a448e028257d0f0d8eebf9ebd40de9914bdbd4c
Sayem <mustakhasansayem@gmail.com>
2024-04-30 18:24:33 +0600

    Error page Added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,39 @@
+import { Link } from "react-router-dom";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/header/Navbar";
 
 
 
 const Error = () => {
     return (
-  <div>
+      <div>
+      
+      <div>
+        
 
-  </div>
+        <section className='flex items-center  p-16  text-gray-800'>
+          <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+            <div className='max-w-md text-center'>
+              <h2 className='mb-8 font-extrabold text-9xl text-gray-400'>
+                <span className='sr-only'>Error</span>404
+              </h2>
+              <p className='text-2xl font-semibold md:text-3xl'>
+                Sorry, we could not find this page.
+              </p>
+              <p className='mt-4 mb-8 text-gray-600'>
+                But do not worry, you can find other things on our homepage
+              </p>
+              <Link to="/"
+               
+                className='px-8 py-3 font-semibold rounded '>
+                Back to homepage
+              </Link>
+            </div>
+          </div>
+        </section>
+        
+      </div>
+    </div>
     );
 };
 
